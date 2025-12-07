92c2fbdac4b23c7bc785b38e8a3cdef7c635b193
Diana <savelievadiana067@gmail.com>
2025-12-07 11:00:16 +0300

    Работа с сохранением влажности

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <script setup>
-import Button from "./components/Button.vue";
 import Stat from "./components/Stat.vue";
 import CitySelect from "./components/CitySelect.vue";
+import { ref } from "vue";
 
 const data = {
   label: "Влажность",
   stat: "90 %",
 };
 
+let savedCity = ref("Moscow");
+
 function getCity(city) {
+  savedCity.value = city;
   console.log(city);
 }
 </script>
 
 <template>
   <main class="main">
+    {{ savedCity }}
     <Stat v-bind="data" />
     <Stat label="Осадки" stat="0 %" />
     <CitySelect @select-city="getCity" />
   </main>
 </template>
+
 <style scoped>
 .main {
   background: var(--color-bg-main);
--- src/components/CitySelect.vue
+++ src/components/CitySelect.vue
@@ -1,18 +1,27 @@
 <script setup>
+import { ref } from "vue";
 import locationIcon from "../assets/Location.svg";
 import Button from "./Button.vue";
 
 const emit = defineEmits(["select-city"]);
 
+let isEdited = ref(false);
+
+function edit() {
+  isEdited.value = true;
+}
+
 function select() {
+  isEdited.value = false;
   emit("select-city", "London");
 }
 </script>
 <template>
-  <Button @click="select()">
+  {{ isEdited }}
+  <Button @click="edit()">
     <img :src="locationIcon" alt="Location" />
     Изменить город
   </Button>
   <input />
-  <Button> Сохранить </Button>
+  <Button @click="select()"> Сохранить </Button>
 </template>
